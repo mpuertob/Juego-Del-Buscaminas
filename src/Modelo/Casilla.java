4c8ee96083094978dd27d842339ab856c2f7d0f0
programacion <49229873+mpuertob@users.noreply.github.com>
2020-03-21 12:55:55 +0100

    cambio

--- src/Modelo/Casilla.java
+++ src/Modelo/Casilla.java
@@ -3,11 +3,14 @@ package Modelo;
 public class Casilla {
 	private Coordenada coordenada;
 	private Tipo tipo;
+	private boolean marcada, desvelada;
 
 	public Casilla(Coordenada coordenada, Tipo tipo) {
 		super();
 		this.coordenada = coordenada;
 		this.tipo = tipo;
+		this.marcada = false;
+		this.desvelada = false;
 	}
 
 	public Coordenada getCoordenada() {
@@ -20,6 +23,22 @@ public class Casilla {
 
 	public Tipo getTipo() {
 		return tipo;
+	}
+
+	public boolean isMarcada() {
+		return marcada;
+	}
+
+	public void setMarcada(boolean marcada) {
+		this.marcada = marcada;
+	}
+
+	public boolean isDesvelada() {
+		return desvelada;
+	}
+
+	public void setDesvelada(boolean desvelada) {
+		this.desvelada = desvelada;
 	}
 
 }
